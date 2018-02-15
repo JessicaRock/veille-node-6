ee3f8d2c510dbfe659cd3c582fb1f1c61c0835dd
Jessica Rock <e1530023@cmaisonneuve.qc.ca>
2018-02-15 12:36:06 -0500

    Formulaire de modification caché

--- public/js/modifier.js
+++ public/js/modifier.js
@@ -1,32 +1,31 @@
 (function() {
 		console.log('click modifier');
+		let aBtnModifier = document.querySelectorAll('.modifier');
 
-//fonction autoexécutante???
-let aBtnModifier = document.querySelectorAll('.modifier');
+			console.log(aBtnModifier);
+		for (elm of aBtnModifier) {
+			elm.addEventListener('click', function(){
 
-for (elm of aBtnModifier) {
-	elm.addEventListener('click', function(){
 
-		console.log('click modifier');
+				let tr = this.parentNode.parentNode;
+				let id = tr.children[0].innerHTML;
+				let prenom = tr.children[1].innerHTML;
+				let nom = tr.children[2].innerHTML;
+				let telephone = tr.children[3].innerHTML;
+				let courriel = tr.children[4].innerHTML;
 
-		let tr = this.parentNode.parentNode;
-		let id = tr.children[0].innerHTML;
-		let prenom = tr.children[1].innerHTML;
-		let nom = tr.children[2].innerHTML;
-		let telephone = tr.children[3].innerHTML;
-		let courriel = tr.children[4].innerHTML;
+				let elmForm = document.getElementById('formModif');
 
-		let elmForm = document.getElementById('formModif');
+				let elmInput = elmForm.querySelectorAll('input');
+				elmInput[0].value = id;
+				elmInput[1].value = prenom;
+				elmInput[2].value = nom;
+				elmInput[3].value = telephone;
+				elmInput[4].value = courriel;
 
-		let elmInput = elmForm.querySelectorAll('input')
-		elmInput[0].value = prenom
-		elmInput[1].value = nom
-		elmInput[2].value = telephone
-		elmInput[3].value = courriel
+				elmForm.submit()
 
-		//elmForm.submit()
+			})
+		}
 
- 	})
- }
-
-})();
+	})();
